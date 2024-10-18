06e3a237a9e6e7a72208a5c19eb7ee961c6c2f14
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-10-18 22:23:38 +0900

    カートアイテムのjotaiからの取得

--- src/pages/ShoppingCartPage.tsx
+++ src/pages/ShoppingCartPage.tsx
@@ -1,10 +1,48 @@
+import { useEffect, useState } from "react";
 import CartItemCard from "../component/featured/ShoppingCartPage/CartItemCart";
 import Layout from "../component/shared/Layout";
 import PrimaryButton from "../component/shared/PrimaryButton";
 import "../style/ShoppingCartPage.css";
+import { userInfoAtom } from "../lib/jotai/atoms/user";
+import { useAtom } from "jotai";
+import { cartInfoAtom } from "../lib/jotai/atoms/cart";
+import { CartInfoType } from "../lib/type/ProductType";
 
 const ShoppingCartPage = () => {
-  const cartItems = Array.from({ length: 10 }, (_, index) => index + 1);
+  // const cartItems = Array.from({ length: 10 }, (_, index) => index + 1);
+
+  const [userInfoJotai] = useAtom(userInfoAtom);
+  console.log("userInfoJotai", userInfoJotai);
+
+  const [cartInfoJotai] = useAtom(cartInfoAtom);
+
+  const [cartItems, setCartItems] = useState<CartInfoType[]>([]);
+  const [sumPrice, setSumPrice] = useState<number>(0);
+  const [shippingFee, setShippingFee] = useState<number>(0);
+
+  const sumAllCartItems = (cartItems: CartInfoType[]) => {
+    //カートに入っているすべての商品の合計値を算出してセットする関数
+    let sum = 0;
+    for (const cartItem of cartItems) {
+      console.log(cartItem);
+      sum += cartItem.product.price * cartItem.quantity;
+    }
+    setSumPrice(sum);
+
+    setShippingFee(sum > 5000 || sum === 0 ? 0 : 500);
+  };
+
+  useEffect(() => {
+    // グローバルステートからログインユーザー情報を取得
+    if (userInfoJotai.userInfo) {
+      // ユーザー情報が入っている場合=ログインしている→ユーザーIDをキーにバックエンドからカートの中身を取得
+    } else {
+      // ユーザー情報がない＝未ログインの場合→ローカルストレージからカートの中身をとってくる
+      console.log("cartInfoJotai", cartInfoJotai);
+      setCartItems(cartInfoJotai.cartItems);
+      sumAllCartItems(cartInfoJotai.cartItems);
+    }
+  }, []);
   return (
     <Layout>
       <div className="shoppingCartPage_container">
@@ -14,7 +52,7 @@ const ShoppingCartPage = () => {
         <div className="shoppingCartPage_content_container">
           <div className="shoppingCartPage_cartItems_container">
             {cartItems.map((cartItem) => {
-              return <CartItemCard key={cartItem} />;
+              return <CartItemCard key={cartItem.product.id} />;
             })}
           </div>
           <div className="shoppingCartPage_settlement_container">
@@ -22,23 +60,25 @@ const ShoppingCartPage = () => {
               <h3>注文内容</h3>
               <p className="settlement-item">
                 <span>小計:</span>
-                <span>¥10,000</span>
+                <span>{sumPrice}</span>
               </p>
               <p className="settlement-item">
                 <span>送料:</span>
-                <span>¥500</span>
+                <span>{shippingFee}</span>
               </p>
               <p className="settlement-total">
                 <span>合計:</span>
-                <span>¥10,500</span>
+                <span>{sumPrice + shippingFee}</span>
               </p>
-              <div className="shoppingCartPage_settlementButton_container">
-                <PrimaryButton
-                  onClick={() => alert("settlement click")}
-                  loading={false}
-                  text={"レジに進む"}
-                />
-              </div>
+              {cartItems.length > 0 && (
+                <div className="shoppingCartPage_settlementButton_container">
+                  <PrimaryButton
+                    onClick={() => alert("settlement click")}
+                    loading={false}
+                    text={"レジに進む"}
+                  />
+                </div>
+              )}
             </div>
           </div>
         </div>
