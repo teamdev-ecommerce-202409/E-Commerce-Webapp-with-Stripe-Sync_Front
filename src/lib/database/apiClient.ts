e89553e84b9ebb34643bd41f7db471a4e1d77c41
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-11-14 09:28:20 +0900

    アクセストークン失効時のリフレッシュトークン活用

--- src/lib/database/apiClient.ts
+++ src/lib/database/apiClient.ts
@@ -1,4 +1,8 @@
 import axios from "axios";
+import { getDefaultStore } from "jotai";
+import { userInfoAtom } from "../jotai/atoms/user";
+
+const store = getDefaultStore();
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -8,4 +12,48 @@ const apiClient = axios.create({
   },
 });
 
+apiClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    console.log("インターセプタ―呼び出し");
+
+    const originalRequest = error.config;
+
+    // トークンの期限切れを示すエラーレスポンスかどうかを確認
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        // リフレッシュトークンを使って新しいアクセストークンを取得
+        const { refresh } = store.get(userInfoAtom);
+        const baseURL = import.meta.env.VITE_API_URL;
+        const response = await axios.post(baseURL + "/token/refresh/", {
+          refresh: refresh,
+        });
+
+        const newAccess = response.data.access;
+        console.log("newAccess", newAccess);
+
+        store.set(userInfoAtom, {
+          ...store.get(userInfoAtom),
+          access: newAccess,
+        });
+
+        originalRequest.headers.Authorization = `Bearer ${newAccess}`;
+        return apiClient(originalRequest);
+      } catch (error) {
+        console.error(error);
+        alert("リフレッシュトークンが無効です。再度ログインが必要です。");
+        store.set(userInfoAtom, null);
+        window.location.reload();
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
 export default apiClient;
